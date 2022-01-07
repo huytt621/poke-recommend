24341c4c26849cca1f42b32ba9f93f6b940ac57b
Huy Tran <huytt621@gmail.com>
2022-01-07 14:10:08 -0800

    Implement basic cosine similarity recommender

--- recommenders.py
+++ recommenders.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas
 from sklearn.neighbors import NearestNeighbors
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 class knn_recommender():
     k = 5
@@ -30,5 +32,34 @@ class knn_recommender():
 
     def get_pokemon(self, index):
         return self.train_data.iloc[index]['name']
-        
 
+class cosine_recommender():
+
+    def __init__(self):
+        self.train_data = None
+        self.similarity_matrix = None
+        self.pokemon_index = None
+        
+    def create(self, train_data):
+        self.train_data = train_data
+        self.pokemon_index = {}
+        vectorizer = CountVectorizer()
+        words = []
+        for index, row in train_data.iterrows():
+            types_and_abilities = ' '.join([row['ability 0'], row['ability 1'], row['ability 2'], row['type 0'], row['type 1']])
+            words.append(types_and_abilities)
+            self.pokemon_index[row['name']] = index
+        word_count = vectorizer.fit_transform(words)
+        self.similarity_matrix = cosine_similarity(word_count, word_count)
+
+    def recommend(self, pokemon_name):
+        pokemon_index = self.get_pokemon_index(pokemon_name)
+        pokemon_row = [(similarity, index) for index, similarity in enumerate(self.similarity_matrix[pokemon_index])]
+        pokemon_row.sort(key=lambda pair: pair[0], reverse=True)
+        return pokemon_row[0:6]
+
+    def get_pokemon_index(self, pokemon_name):
+        return self.pokemon_index[pokemon_name]
+
+    def get_pokemon(self, index):
+        return self.train_data.iloc[index]['name']
